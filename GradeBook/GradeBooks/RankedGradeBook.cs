947b86dc33ec7bb9656bacc7dc4b811f42404c02
mintou <darcher@athene.bm>
2018-04-25 16:14:45 -0300

    override statistics methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
@@ -27,7 +28,7 @@ namespace GradeBook.GradeBooks
             grades.Sort();
 
             double GradeRank = 0;
-            for(int i = 0; i< grades.Count; i++)
+            for (int i = 0; i < grades.Count; i++)
             {
                 if (averageGrade > grades[i])
                 {
@@ -50,5 +51,27 @@ namespace GradeBook.GradeBooks
 
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
